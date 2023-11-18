d0df1bcf97693b726cf4baa2b52e10f300dd2e95
NiebAnupat <anupat.k64@rsu.ac.th>
2023-11-18 19:41:37 +0700

    📦 chore(payment.controller.ts): add payment controller to handle payment-related operations
    This commit adds a new file `payment.controller.ts` which contains the implementation of the PaymentController class. The PaymentController is responsible for handling various payment-related operations.
    
    The PaymentController class includes the following methods:
    
    - `create`: This method is used to create a new payment. It accepts a `CreatePaymentDto` object and an uploaded file as parameters. It generates a unique payment ID, saves the uploaded file to a cloud storage service, and creates a new payment record in the database.
    
    - `findAll`: This method is used to retrieve all payments for a specific user. It accepts the `userId` as a parameter and returns an array of payment records.
    
    - `findOne`: This method is used to retrieve a specific payment for a user. It accepts the `payId` and `userId` as query parameters and returns the corresponding payment record.
    
    - `findOneImg`: This method is used to retrieve the image associated with a payment. It accepts the `filename` as a parameter and returns the image file.
    
    - `update`: This method is used to update a payment. It accepts the `payId`, `userId`, and an optional uploaded file as parameters. If a new file is provided, it replaces the existing file in the cloud storage service and updates the payment record in the database.
    
    - `remove`: This method is used to remove a payment. It accepts the `payId` and `userId` as query parameters and deletes the corresponding payment record.
    
    - `verify`: This method is used to verify a payment. It accepts the `payId` and `userId` as query parameters. It reads the QR code from the payment slip image, sends it to a third-party API for verification, and updates the payment record in the database if the verification is successful.
    
    The PaymentController class also includes constructor injection of the `PaymentService`, `UserService`, and `CloudStorageService` dependencies.
    
    This commit also imports various modules and classes required for the implementation of the PaymentController.

---
+++ Backend/src/payment/payment.controller.ts
@@ -0,0 +1,176 @@
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+    UseInterceptors,
+    UploadedFile,
+    Res, Query, NotFoundException, BadRequestException, ForbiddenException
+} from '@nestjs/common';
+import {Response} from "express";
+import {PaymentService} from './payment.service';
+import {CreatePaymentDto} from './dto/create-payment.dto';
+import {UpdatePaymentDto} from './dto/update-payment.dto';
+import {FileInterceptor} from "@nestjs/platform-express";
+import {CloudStorageService} from "../cloud-storage/cloud-storage.service";
+import {v4} from "uuid";
+import {readFileSync, unlinkSync} from "fs";
+import {ObjectId} from "typeorm";
+import {UserService} from "../user/user.service";
+import {UpdateUserDto} from "../user/dto/update-user.dto";
+import axios from "axios";
+import * as QrCode from 'qrcode-reader';
+import Jimp from "jimp";
+
+@Controller('payment')
+export class PaymentController {
+    private qr = new QrCode()
+
+    constructor(
+        private readonly paymentService: PaymentService,
+        private readonly userService: UserService,
+        private readonly cloudStorageService: CloudStorageService,
+    ) {
+    }
+
+    @Post("/")
+    @UseInterceptors(FileInterceptor('slipImage'))
+    async create(@Body() createPaymentDto: CreatePaymentDto, @UploadedFile() file: Express.Multer.File) {
+
+        createPaymentDto.timestamp = new Date().toISOString()
+        const payId = v4()
+        const ext = file.originalname.split('.').pop()
+        const path = `pay-slip/${payId}.${ext}`
+        // const url = this.cloudStorageService.save(path, file.buffer)
+        const buffer = readFileSync(file.path)
+
+
+        // if (!qrCode.result) throw new BadRequestException("invalid slip")
+        // console.log('QR Code content:', qrCode.result);
+
+        createPaymentDto.payId = payId
+        createPaymentDto.filename = `${payId}.${ext}`
+        // createPaymentDto.qrCode = qrCode.result
+
+        await this.cloudStorageService.save(path, file.mimetype, buffer, [{
+            payId,
+        }])
+        unlinkSync(file.path)
+
+
+        return this.paymentService.create(createPaymentDto);
+
+    }
+
+    @Get(':userId')
+    findAll(@Param('userId') userId: ObjectId) {
+        return this.paymentService.findAll(userId);
+    }
+
+    @Get()
+    findOne(@Query() query: { payId: string, userId: ObjectId }) {
+        return this.paymentService.findOne(query.payId, query.userId);
+    }
+
+
+    @Get('img/:filename')
+    async findOneImg(@Param('filename') filename: string, @Res() res: Response) {
+        const storageFile = await this.paymentService.findOneImg(`pay-slip/${filename}`);
+        res.setHeader("Content-Type", storageFile.contentType);
+        res.setHeader("Content-Length", storageFile.buffer.length)
+        res.setHeader("Content-Disposition", `inline; filename=${filename}`)
+        res.setHeader("Cache-Control", "max-age=1d");
+        return res.end(storageFile.buffer);
+    }
+
+
+    @Patch()
+    @UseInterceptors(FileInterceptor('slipImage'))
+    async update(@Query() query: { payId: string, userId: ObjectId }, @Body() updatePaymentDto: UpdatePaymentDto, @UploadedFile() file: Express.Multer.File) {
+        if (file) {
+
+            // when update image , delete old image in cloud storage
+            const payment = await this.paymentService.findOne(query.payId, query.userId)
+            if (!payment) throw new NotFoundException("payment not found")
+            await this.cloudStorageService.delete(`pay-slip/${payment.filename}`)
+
+            // save new image to cloud storage
+            const ext = file.originalname.split('.').pop()
+            const path = `pay-slip/${query.payId}.${ext}`
+            const buffer = readFileSync(file.path)
+            await this.cloudStorageService.save(path, file.mimetype, buffer, [{
+                payId: query.payId,
+            }])
+            unlinkSync(file.path)
+            updatePaymentDto.filename = `${query.payId}.${ext}`
+        }
+        return this.paymentService.update({userId: query.userId, payId: query.payId}, updatePaymentDto);
+    }                        
+
+    @Delete()
+    remove(@Query() query: { payId: string, userId: ObjectId }) {
+        return this.paymentService.remove({userId: query.userId, payId: query.payId});
+    }
+
+    @Patch('verify')
+    async verify(@Query() query: { payId: string, userId: ObjectId }) {
+        const {SLIPOK_API_URL, SLIPOK_BRAND_ID, SLIPOK_API_KEY} = process.env
+        const user = await this.userService.findOne(query.userId)
+        const payment = user.paymentData.find(payment => payment.payId === query.payId)
+        if (!payment) throw new NotFoundException("payment not found")
+        const slipImg = await this.paymentService.findOneImg(`pay-slip/${payment.filename}`);
+
+        const buffer = slipImg.buffer
+
+        const bitmap = await Jimp.read(buffer).then(image => image.bitmap).catch(e => {
+            throw new BadRequestException({
+                success: false,
+                message: "invalid slip"
+            })
+        })
+
+        let qrString = ""
+        this.qr.callback = function (err, value) {
+            if (err) {
+                throw new BadRequestException({
+                    success: false,
+                    message: "invalid slip"
+                })
+            }
+            qrString = value.result
+        }
+        await this.qr.decode(bitmap)
+        const res = await axios.post(`${SLIPOK_API_URL}/${SLIPOK_BRAND_ID}`, {
+            data: qrString,
+        }, {
+            headers: {
+                "x-authorization": SLIPOK_API_KEY,
+            }
+        }).then(res => res.data).catch(e => {
+            console.log(e.response.data)
+        })
+
+
+        if (res.success) {
+            if (res.data.amount !== payment.amount) throw new ForbiddenException(`invalid amount, expected ${payment.amount} but got ${res.data.amount}`)
+
+            payment.isVerified = true
+            const updateUserDto: UpdateUserDto = {
+                paymentData: user.paymentData
+            }
+            await this.userService.update(user._id, updateUserDto)
+            return {
+                success: true,
+                message: "payment verified"
+            }
+        } else {
+            throw new BadRequestException({
+                success: false,
+                message: "invalid slip"
+            })
+        }
+    }
+}
